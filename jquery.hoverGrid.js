050036daefd8371193b40ca74027c509db07183b
Carl Woodhouse <nothing.is.forever@gmail.com>
2012-07-20 13:58:24 +0100

    ditched the zoom thing

--- jquery.hoverGrid.js
+++ jquery.hoverGrid.js
@@ -8,9 +8,7 @@
 (function( $ ){
   $.fn.hoverGrid = function( options ) {  
 	var settings = $.extend( {
-      'itemClass' : '.item',
-	  'zoom' : 1,
-	  'move' : -20
+      'itemClass' : '.item'
     }, options);
   
     return this.each(function() {       
@@ -18,22 +16,10 @@
 		hoverGrid.addClass('hover-grid');
 		hoverGrid.find(settings.itemClass).addClass('hover-grid-item');
 		
-		var hoverZoom = settings.zoom;
-		var hoverMove = settings.move;
-		var item = hoverGrid.find(settings.itemClass).first();
-		var itemOriginalWidth = item.width();
-		var itemOriginalHeight = item.height();
-		var itemWidth = item.width() * hoverZoom;
-		var itemHeight = item.height() * hoverZoom;
-
 		$(hoverGrid).find(settings.itemClass).hover(function () {
-			$(this).find('img').stop(false, true).stop().animate({ 'width': itemWidth, 'height': itemHeight, 'top': hoverMove, 'left': hoverMove }, { duration: 200 });
-
 			$(this).find('div.caption').stop(false, true).fadeIn(200);
 		},
         function () {
-			$(this).find('img').stop(false, true).stop().animate({ 'width': itemOriginalWidth, 'height': itemOriginalHeight, 'top': '0', 'left': '0' }, { duration: 100 });
-
             $(this).find('div.caption').stop(false, true).fadeOut(200);
         });
     });
