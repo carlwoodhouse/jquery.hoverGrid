d75c97437f6a9cbfe0f9c039e1a8b8ba8f14d46f
Carl Woodhouse <nothing.is.forever@gmail.com>
2012-07-20 13:07:38 +0100

    tidy up zoom & move

--- jquery.hoverGrid.js
+++ jquery.hoverGrid.js
@@ -17,25 +17,23 @@
 		var hoverGrid = $(this);
 		hoverGrid.addClass('hover-grid');
 		hoverGrid.find(settings.itemClass).addClass('hover-grid-item');
+		
+		var hoverZoom = settings.zoom;
+		var hoverMove = settings.move;
+		var item = hoverGrid.find(settings.itemClass);
+		var itemWidth = item.width() * hoverZoom;
+		var itemHeight = item.height() * hoverZoom;
 
 		$(hoverGrid).find(settings.itemClass).hover(function () {
-			var zoom = settings.zoom;
-			var move = settings.move;
-			var item = $(this);
-			
-			//Set the width and height according to the zoom percentage
-			var width = $(item).width() * zoom;
-			var height = $(item).height() * zoom;
-
 			//Move and zoom the image
-			$(this).find('img').stop(false, true).stop().animate({ 'width': width, 'height': height, 'top': move, 'left': move }, { duration: 200 });
+			$(this).find('img').stop(false, true).stop().animate({ 'width': itemWidth, 'height': itemHeight, 'top': hoverMove, 'left': hoverMove }, { duration: 200 });
 
 			//Display the caption
 			$(this).find('div.caption').stop(false, true).fadeIn(200);
 		},
         function () {
             //Reset the image
-            $(this).find('img').stop(false, true).stop().animate({ 'width': $('.item').width(), 'height': $('.item').height(), 'top': '0', 'left': '0' }, { duration: 100 });
+            $(this).find('img').stop(false, true).stop().animate({ 'width': itemWidth, 'height': itemHeight, 'top': '0', 'left': '0' }, { duration: 100 });
 
             //Hide the caption
             $(this).find('div.caption').stop(false, true).fadeOut(200);
