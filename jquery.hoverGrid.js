57d6b3a0202d3dbf3d30776a58ca508bbcce4f47
Carl Woodhouse <nothing.is.forever@gmail.com>
2012-07-20 13:14:37 +0100

    still trying to make it work

--- jquery.hoverGrid.js
+++ jquery.hoverGrid.js
@@ -23,19 +23,24 @@
 		var item = hoverGrid.find(settings.itemClass);
 		var itemOriginalWidth = item.width();
 		var itemOriginalHeight = item.height();
+		var doHoverZoom = (zoom != 0 && move != 0)
 		var itemWidth = item.width() * hoverZoom;
 		var itemHeight = item.height() * hoverZoom;
 
 		$(hoverGrid).find(settings.itemClass).hover(function () {
-			//Move and zoom the image
-			$(this).find('img').stop(false, true).stop().animate({ 'width': itemWidth, 'height': itemHeight, 'top': hoverMove, 'left': hoverMove }, { duration: 200 });
+			if(doHoverZoom)
+			{
+				$(this).find('img').stop(false, true).stop().animate({ 'width': itemWidth, 'height': itemHeight, 'top': hoverMove, 'left': hoverMove }, { duration: 200 });
+			}
 
 			//Display the caption
 			$(this).find('div.caption').stop(false, true).fadeIn(200);
 		},
         function () {
-            //Reset the image
-            $(this).find('img').stop(false, true).stop().animate({ 'width': itemOriginalWidth, 'height': itemOriginalHeight, 'top': '0', 'left': '0' }, { duration: 100 });
+            if(doHoverZoom)
+			{
+				$(this).find('img').stop(false, true).stop().animate({ 'width': itemOriginalWidth, 'height': itemOriginalHeight, 'top': '0', 'left': '0' }, { duration: 100 });
+			}
 
             //Hide the caption
             $(this).find('div.caption').stop(false, true).fadeOut(200);
